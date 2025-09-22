1019356a242b61bb70d9211dbe1ef155f40f3f8d
Mikles64 <miklesarwing@gmail.com>
2025-09-22 08:45:53 -0600

    Clase11

--- Clase11/Clase11.java
+++ Clase11/Clase11.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 
-public class Clase11{
+public class Clase11{ 
     public static void main(String[] args) throws Exception
     {
         BufferedReader texto = new BufferedReader(new FileReader("El_viejo_y_el_mar.txt"));
@@ -10,14 +10,29 @@ public class Clase11{
         HashMap<Character, Integer> contador = new HashMap<>();
         String st;
         while ((st = texto.readLine()) != null) {
-            // contar caracteres de cada linea
+            // Contar caracteres de cada linea
             for (char caracter : st.toCharArray()) {
                 contador.put(caracter, contador.getOrDefault(caracter, 0) + 1);
             }
         }
         texto.close();
  
-        System.out.println("Numero de caracteres distintos: " + contador.size());
-        System.out.println("Mapa de caracteres y ocurrencias: " + contador);
+        /*System.out.println("Numero de caracteres distintos: " + contador.size());
+        System.out.println("Mapa de caracteres y ocurrencias: " + contador);*/
+
+        ArrayList<HashMap.Entry<Character, Integer>> lista = new ArrayList<>(contador.entrySet());
+            // Comparator para ordenar de mayor a menor
+            Comparator<HashMap.Entry<Character, Integer>> comparador = new Comparator<HashMap.Entry<Character, Integer>>() {
+                @Override
+                public int compare(HashMap.Entry<Character, Integer> a, HashMap.Entry<Character, Integer> b) {
+                    return b.getValue().compareTo(a.getValue());
+                }
+            };
+            lista.sort(comparador);
+ 
+            System.out.println("Caracteres ordenados:");
+            for (HashMap.Entry<Character, Integer> entry : lista) {
+                System.out.println(entry.getKey() + ": " + entry.getValue());
+            }
     }
 }
